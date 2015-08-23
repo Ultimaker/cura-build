4d3368ca5563fbbefb7ec8c78e77023b07cfc18e
Grey Christoforo <greysAcademicCode@users.noreply.github.com>
2015-08-23 15:08:07 +0100

    correctly install plugins, build from release tarballs

--- ubuntu-15.04-build-script.sh
+++ ubuntu-15.04-build-script.sh
@@ -1,17 +1,18 @@
-#!/bin/bash
+#/bin/bash
 # This is a script which get the latest git repo and build them.
 #
 # Tested under ubuntu 15.04, lower versions don't have PyQT 5.2.1 which is required by cura
 
 cd ~
-mkdir dev && cd dev
+mkdir -p dev && cd dev
 
 sudo apt-get install -y git cmake cmake-gui autoconf libtool python3-setuptools curl python3-pyqt5.* python3-numpy qml-module-qtquick-controls
-git clone https://github.com/Ultimaker/Cura.git
-git clone https://github.com/Ultimaker/Uranium.git
-git clone https://github.com/Ultimaker/CuraEngine.git
-git clone https://github.com/Ultimaker/libArcus
-git clone https://github.com/Ultimaker/protobuf.git
+curl -L https://github.com/Ultimaker/Cura/archive/15.06.03.tar.gz > Cura.tar.gz
+curl -L https://github.com/Ultimaker/Uranium/archive/15.06.03.tar.gz > Uranium.tar.gz
+curl -L https://github.com/Ultimaker/CuraEngine/archive/15.06.03.tar.gz > CuraEngine.tar.gz
+curl -L https://github.com/Ultimaker/libArcus/archive/15.06.03.tar.gz > libArcus.tar.gz
+curl -L https://github.com/Ultimaker/protobuf/archive/15.06.03.tar.gz > protobuf.tar.gz
+for file in *.tar.gz; do mkdir -p ${file%%.*}; tar -xvf $file --strip-components=1 -C ${file%%.*}; done
 
 cd protobuf
 ./autogen.sh
@@ -43,7 +44,10 @@ cmake .. -DPYTHON_SITE_PACKAGES_DIR=/usr/lib/python3.4/dist-packages -DURANIUM_P
 sudo make install
 cd ../..
 
-cp -rv Uranium/resources/* Cura/resources/
-sudo ln -s $PWD/CuraEngine/build/CuraEngine /usr/bin/CuraEngine
+#cp -rv Uranium/resources/* Cura/resources/
+ln -s $PWD/Uranium/resources/* Cura/resources/.
+ln -s $PWD/Uranium/plugins/* Cura/plugins/.
+mkdir bin
+ln -s $PWD/CuraEngine/build/CuraEngine bin/CuraEngine
 cd Cura
 python3 cura_app.py
