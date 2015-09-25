ce2f25e062348f67decbf794c8fd080cee43638d
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-09-25 16:42:29 +0200

    Make it possible to build 64 bit Windows builds

---
+++ env_win64.bat
@@ -0,0 +1,6 @@
+set PYTHONPATH=%cd%\inst\lib\python3.4\site-packages
+set DXSDK_DIR=C:\Program Files\Microsoft DirectX SDK (June 2010)\
+set PATH=%cd%\inst\bin;%PATH%
+set CFLAGS=-m64
+set CXXFLAGS=-m64
+set LDFLAGS=-m64
