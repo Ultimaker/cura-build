89b55cfc7bbe80dd2aad2629e4d8df727b8ef798
Arjen Hiemstra <a.hiemstra@ultimaker.com>
2016-01-14 13:40:12 +0100

    Remove DirectX SDK environment variable
    DXSDK is only needed when building Qt with ANGLE, which we currently
    do not support.

--- env_win64.bat
+++ env_win64.bat
@@ -1,5 +1,4 @@
 set PYTHONPATH=%cd%\inst\lib\python3.4\site-packages
-set DXSDK_DIR=C:\Program Files\Microsoft DirectX SDK (June 2010)\
 set PATH=%cd%\inst\bin;%PATH%
 set CFLAGS=-m64
 set CXXFLAGS=-m64
