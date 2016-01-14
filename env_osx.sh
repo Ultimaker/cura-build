4fee312a13254bfeb82a7cc43fee781fcdc438ee
Arjen Hiemstra <a.hiemstra@ultimaker.com>
2016-01-14 15:33:45 +0100

    Add /usr/local/include to the include path for OSX
    OSX 10.11 removed the OpenSSL headers, but python's ssl module
    does not build without it. The ssl module is required for setuptools
    so we need to make sure it builds even on OSX 10.11. Brew installs
    to /usr/local/include so that makes sure a brew-installed OpenSSL
    can be used.

--- env_osx.sh
+++ env_osx.sh
@@ -3,3 +3,4 @@
 export MACOSX_DEPLOYMENT_TARGET=10.7
 export CC=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang
 export CXX=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++
+export CFLAGS=-I/usr/local/include
