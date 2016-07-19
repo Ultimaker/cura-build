9dace7eb11b476e032d09b5fc934bbd671d3fd4d
Arjen Hiemstra <ahiemstra@heimr.nl>
2016-07-19 17:26:12 +0200

    Refactor python module find files to use a common function
    This way we can much more easily add find files for other modules

--- cmake/FindPyQt.cmake
+++ cmake/FindPyQt.cmake
@@ -1,24 +1,8 @@
 # Copyright 2016 Ultimaker B.V.
 
-find_package(PythonInterp 3.4.0 QUIET)
+include(FindPythonPackage)
 
-if(PythonInterp_FOUND)
-    execute_process(
-        COMMAND ${PYTHON_EXECUTABLE} -c "import PyQt5.QtCore; print(PyQt5.QtCore.__file__, PyQt5.QtCore.PYQT_VERSION_STR)"
-        RESULT_VARIABLE _findPyQt_status
-        OUTPUT_VARIABLE _findPyQt_output
-        ERROR_QUIET
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-endif()
-
-set(PyQt_CORE_MODULE "PyQt_CORE_MODULE_NOTFOUND")
-set(PyQt_VERSION_STR "PyQt_VERSION_STR_NOTFOUND")
-if(_findPyQt_status EQUAL 0)
-    string(REGEX MATCH "[^ ]+" PyQt_CORE_MODULE ${_findPyQt_output})
-    string(REGEX MATCH " .+" PyQt_VERSION_STR ${_findPyQt_output})
-    string(STRIP ${PyQt_VERSION_STR} PyQt_VERSION_STR)
-endif()
+FindPythonPackage(PACKAGE_NAME "PyQt" MODULE_NAME "PyQt5.QtCore" VERSION_PROPERTY "PYQT_VERSION_STR")
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(PyQt REQUIRED_VARS PyQt_CORE_MODULE VERSION_VAR PyQt_VERSION_STR)
+find_package_handle_standard_args(PyQt REQUIRED_VARS PyQt_PATH VERSION_VAR PyQt_VERSION)
--- cmake/FindSciPy.cmake
+++ cmake/FindSciPy.cmake
@@ -1,24 +1,8 @@
 # Copyright 2016 Ultimaker B.V.
 
-find_package(PythonInterp 3.4.0 QUIET)
+include(FindPythonPackage)
 
-if(PythonInterp_FOUND)
-    execute_process(
-        COMMAND ${PYTHON_EXECUTABLE} -c "import scipy; print(scipy.__file__, scipy.__version__)"
-        RESULT_VARIABLE _findSciPy_status
-        OUTPUT_VARIABLE _findSciPy_output
-        ERROR_QUIET
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-endif()
-
-set(SciPy_MODULE "SciPy_MODULE_NOTFOUND")
-set(SciPy_VERSION_STR "SciPy_VERSION_STR_NOTFOUND")
-if(_findSciPy_status EQUAL 0)
-    string(REGEX MATCH "[^ ]+" SciPy_MODULE ${_findSciPy_output})
-    string(REGEX MATCH " (.+)" SciPy_VERSION_STR ${_findSciPy_output})
-    string(STRIP ${SciPy_VERSION_STR} SciPy_VERSION_STR)
-endif()
+FindPythonPackage(PACKAGE_NAME "SciPy" MODULE_NAME "scipy")
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SciPy REQUIRED_VARS SciPy_MODULE VERSION_VAR SciPy_VERSION_STR)
+find_package_handle_standard_args(SciPy REQUIRED_VARS SciPy_PATH VERSION_VAR SciPy_VERSION)
