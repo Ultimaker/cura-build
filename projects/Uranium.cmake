5d73e5d7f1b0b5b08489cafe18360f7682765baf
Arjen Hiemstra <ahiemstra@heimr.nl>
2016-07-19 17:25:21 +0200

    Add project files for Cura, CuraEngine and Uranium

---
+++ projects/Uranium.cmake
@@ -0,0 +1,10 @@
+find_package(PythonInterp 3.5.0 REQUIRED)
+find_package(Qt5 5.6.0 REQUIRED Core Qml Quick Widgets)
+find_package(PyQt 5.6 REQUIRED)
+find_package(SciPy 0.17 REQUIRED)
+
+ExternalProject_Add(Uranium
+    GIT_REPOSITORY https://github.com/ultimaker/Uranium
+    GIT_TAG origin/${TAG_OR_BRANCH}
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX}
+)
