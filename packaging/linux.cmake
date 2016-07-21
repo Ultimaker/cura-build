ceef674dcf3478efd4b7dc5282a5c62a2e1a7758
Arjen Hiemstra <ahiemstra@heimr.nl>
2016-07-21 08:50:15 +0200

    Add a working cx_Freeze script for Linux

--- packaging/linux.cmake
+++ packaging/linux.cmake
@@ -1,4 +1,14 @@
 find_package(cx_Freeze 4.3 REQUIRED)
 # find_package(AppImageKit REQUIRED)
+
+configure_file(${CMAKE_CURRENT_LIST_DIR}/setup_linux.py.in setup.py @ONLY)
+
+add_custom_command(
+    TARGET packaging POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E remove_directory ${CMAKE_BINARY_DIR}/package
+    COMMAND ${PYTHON_EXECUTABLE} setup.py build
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    VERBATIM
+)
 #
 #
