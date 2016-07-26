5e677b952e335898efd1f3fbb3a57b822bb5606b
Arjen Hiemstra <a.hiemstra@ultimaker.com>
2016-07-26 18:54:14 +0200

    Implement packaging for OSX with cx_Freeze

--- packaging/osx.cmake
+++ packaging/osx.cmake
@@ -1,16 +1,11 @@
 find_package(cx_freeze 5.0 REQUIRED)
 
+file(WRITE ${CMAKE_BINARY_DIR}/qt.conf "# Required for Qt to work properly")
 configure_file(${CMAKE_CURRENT_LIST_DIR}/setup_osx.py.in setup.py @ONLY)
 
 add_custom_command(
     TARGET packaging POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E remove_directory ${CMAKE_BINARY_DIR}/package
-    COMMAND ${PYTHON_EXECUTABLE} setup.py build
+    COMMAND ${CMAKE_COMMAND} -E remove_directory ${CMAKE_BINARY_DIR}/build
+    COMMAND ${PYTHON_EXECUTABLE} setup.py bdist_dmg
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
 )
-
-install(DIRECTORY ${CMAKE_BINARY_DIR}/package/Cura.app DESTINATION "." USE_SOURCE_PERMISSIONS)
-
-set(CPACK_GENERATOR "DragNDrop")
-
-add_dependencies(signing package)
