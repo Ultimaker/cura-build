755b6ff6da5a64519ff843cd1c04029e828473ac
Arjen Hiemstra <a.hiemstra@ultimaker.com>
2016-07-26 18:52:33 +0200

    Ensure we set CMAKE_PREFIX_PATH to the right path for OSX to find the right Python

--- projects/Cura-PostProcessing.cmake
+++ projects/Cura-PostProcessing.cmake
@@ -2,7 +2,7 @@
 ExternalProject_Add(Cura-PostProcessing
     GIT_REPOSITORY https://github.com/nallath/PostProcessingPlugin
     GIT_TAG origin/${TAG_OR_BRANCH}
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX}
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX} -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX}
 )
 
 SetProjectDependencies(TARGET Cura-PostProcessing DEPENDS Cura)
