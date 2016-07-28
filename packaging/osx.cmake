f3cb21c4fb5f97b550cb656ec36e50d206f6c358
Arjen Hiemstra <a.hiemstra@ultimaker.com>
2016-07-28 14:46:36 +0200

    Finish up creating an app bundle for OSX

--- packaging/osx.cmake
+++ packaging/osx.cmake
@@ -2,10 +2,11 @@ find_package(cx_freeze 5.0 REQUIRED)
 
 file(WRITE ${CMAKE_BINARY_DIR}/qt.conf "# Required for Qt to work properly")
 configure_file(${CMAKE_CURRENT_LIST_DIR}/setup_osx.py.in setup.py @ONLY)
+configure_file(${CMAKE_CURRENT_LIST_DIR}/Info.plist.in Info.plist @ONLY)
 
 add_custom_command(
     TARGET packaging POST_BUILD
     COMMAND ${CMAKE_COMMAND} -E remove_directory ${CMAKE_BINARY_DIR}/build
-    COMMAND ${PYTHON_EXECUTABLE} setup.py bdist_dmg
+    COMMAND ${PYTHON_EXECUTABLE} setup.py bdist_mac
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
 )
