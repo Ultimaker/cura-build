b1b52ed558532dc7af0bf0d0007039da11ae14d2
Arjen Hiemstra <a.hiemstra@ultimaker.com>
2016-07-28 14:46:59 +0200

    Create a .dmg in the signing step
    We need to sign all executables in the bundle before packaging. This allows us to do that.

--- signing/osx.cmake
+++ signing/osx.cmake
@@ -1,6 +1,22 @@
+set(OSX_CODESIGN_IDENTITY "Mac Developer" CACHE STRING "Identity to use for code signing")
 
 # add_custom_command(
 #     TARGET signing
 #     COMMAND
 #     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
 # )
+
+install(DIRECTORY ${CMAKE_BINARY_DIR}/build/Cura.app DESTINATION "." USE_SOURCE_PERMISSIONS)
+
+set(CPACK_PACKAGE_NAME "Cura")
+set(CPACK_PACKAGE_VENDOR "Ultimaker")
+set(CPACK_PACKAGE_VERSION_MAJOR ${CURA_MAJOR_VERSION})
+set(CPACK_PACKAGE_VERSION_MINOR ${CURA_MINOR_VERSION})
+set(CPACK_PACKAGE_VERSION_PATCH ${CURA_PATCH_VERSION})
+set(CPACK_PACKAGE_VERSION ${CURA_VERSION})
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Cura 3D Printing Software")
+set(CPACK_PACKAGE_CONTACT "Arjen Hiemstra <a.hiemstra@ultimaker.com>")
+
+set(CPACK_GENERATOR "DragNDrop")
+
+include(CPack)
