057df46cab96079f0945c00669e222d1cdc3fbdf
Arjen Hiemstra <a.hiemstra@ultimaker.com>
2016-08-11 10:21:45 +0200

    Make signing on OSX work properly

--- signing/osx.cmake
+++ signing/osx.cmake
@@ -1,10 +1,16 @@
 set(OSX_CODESIGN_IDENTITY "Mac Developer" CACHE STRING "Identity to use for code signing")
 
-# add_custom_command(
-#     TARGET signing
-#     COMMAND
-#     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-# )
+add_custom_command(
+    TARGET signing PRE_BUILD
+    COMMAND ${CMAKE_CURRENT_LIST_DIR}/fix_codesign_issues.sh
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/build
+)
+
+add_custom_command(
+    TARGET signing PRE_BUILD
+    COMMAND codesign -s ${OSX_CODESIGN_IDENTITY} --deep Cura.app
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/build
+)
 
 install(DIRECTORY ${CMAKE_BINARY_DIR}/build/Cura.app DESTINATION "." USE_SOURCE_PERMISSIONS)
 
