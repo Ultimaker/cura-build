d0ccef3570b5e0fd29d72a3788d02e8bd896be67
Arjen Hiemstra <ahiemstra@heimr.nl>
2016-10-17 14:08:26 +0200

    Add LD_LIBRARY_PATH to startup script so CuraEngine starts

--- packaging/cura.sh
+++ packaging/cura.sh
@@ -2,6 +2,7 @@
 
 scriptdir=$(dirname $0)
 
+export LD_LIBRARY_PATH=$scriptdir
 export QT_PLUGIN_PATH=$scriptdir/qt/plugins
 export QML2_IMPORT_PATH=$scriptdir/qt/qml
 export QT_QPA_FONTDIR=$scriptdir/qt/fonts
