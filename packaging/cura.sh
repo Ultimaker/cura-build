3994fb1e7b555a8f2842f4dacd542b4cb08d654a
Arjen Hiemstra <ahiemstra@heimr.nl>
2016-10-24 15:23:01 +0200

    Add QT_XKB_CONFIG_ROOT export to the start script
    Otherwise keyboard input does not seem to work

--- packaging/cura.sh
+++ packaging/cura.sh
@@ -5,6 +5,7 @@ scriptdir=$(dirname $0)
 export LD_LIBRARY_PATH=$scriptdir
 export QT_PLUGIN_PATH=$scriptdir/qt/plugins
 export QML2_IMPORT_PATH=$scriptdir/qt/qml
-export QT_QPA_FONTDIR=$scriptdir/qt/fonts
+export QT_QPA_FONTDIR=/usr/share/fonts
+export QT_XKB_CONFIG_ROOT=/usr/share/X11/xkb
 
 cura $@
