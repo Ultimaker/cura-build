5a6ce239aadbfe3f0b29492fcc9583c70337a6ac
Arjen Hiemstra <ahiemstra@heimr.nl>
2016-12-15 15:47:54 +0100

    Remove UM3 Network Printing project file since it has been merged
    Contributes to CURA-2947

--- projects/Cura-UM3NetworkPrinting.cmake
+++
@@ -1,8 +0,0 @@
-
-ExternalProject_Add(Cura-UM3NetworkPrinting
-    GIT_REPOSITORY https://github.com/Ultimaker/UM3NetworkPrintingPlugin
-    GIT_TAG origin/${TAG_OR_BRANCH}
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX} -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
-)
-
-SetProjectDependencies(TARGET Cura-UM3NetworkPrinting DEPENDS Cura)
