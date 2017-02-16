f80243d1f07a34713463ad174b7cf81a4f2b03a2
Arjen Hiemstra <a.hiemstra@ultimaker.com>
2017-02-16 17:42:59 +0100

    Use the right location for Arcus on Window

--- projects/CuraEngine.cmake
+++ projects/CuraEngine.cmake
@@ -1,17 +1,13 @@
-if(BUILD_OS_WINDOWS)
-    # Helping CMake on Windows to find Protobuf...
-    # Otherwise it claims there is no Protobuf, but there is!
-    set(Protobuf_LIBRARY_RELEASE ${CMAKE_PREFIX_PATH}/lib/libprotobuf.a )
-    set(Protobuf_LITE_LIBRARY_RELEASE ${CMAKE_PREFIX_PATH}/lib/libprotobuf-lite.a )
-    set(Protobuf_PROTOC_LIBRARY_RELEASE ${CMAKE_PREFIX_PATH}/lib/libprotoc.a )
-endif()
-
 find_package(Arcus 1.1 REQUIRED)
+
+if(BUILD_OS_WINDOWS)
+	set(extra_cmake_args -DArcus_DIR=${CMAKE_PREFIX_PATH}/lib-mingw/cmake/Arcus)
+endif()
 
 ExternalProject_Add(CuraEngine
     GIT_REPOSITORY https://github.com/ultimaker/CuraEngine
     GIT_TAG origin/${TAG_OR_BRANCH}
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX} -DCURA_ENGINE_VERSION=${CURA_VERSION} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH} -G ${CMAKE_GENERATOR}
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX} -DCURA_ENGINE_VERSION=${CURA_VERSION} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH} -G ${CMAKE_GENERATOR} ${extra_cmake_args}
 )
 
 SetProjectDependencies(TARGET CuraEngine)
