918e92173e2e70c68fb5fecd6af7218a57ddfb42
fieldOfView <aldo@fieldofview.com>
2017-05-17 14:26:37 +0200

    Remove Doodle3d plugin package
    The package has not been functional for several versions of Cura. Cura refuses to load the plugin. There is no use in packaging it until it is fixed.

--- projects/Cura-Doodle3D.cmake
+++
@@ -1,8 +0,0 @@
-
-ExternalProject_Add(Cura-Doodle3D
-    GIT_REPOSITORY https://github.com/Doodle3D/Doodle3D-cura-plugin
-    GIT_TAG origin/master
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX} -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
-)
-
-SetProjectDependencies(TARGET Cura-Doodle3D DEPENDS Cura)
