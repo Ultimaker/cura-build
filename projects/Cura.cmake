ab87d78790cca61f9a75bb13006188ec49f64c58
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2017-12-08 18:08:12 +0100

    DO NOT MERGE: Get sources from WIP branch

--- projects/Cura.cmake
+++ projects/Cura.cmake
@@ -1,8 +1,8 @@
 option(CURA_ENABLE_DEBUGMODE "Enable crash handler and other debug options in Cura" OFF)
 
 ExternalProject_Add(Cura
-    GIT_REPOSITORY https://github.com/ultimaker/Cura
-    GIT_TAG origin/${TAG_OR_BRANCH}
+    GIT_REPOSITORY https://github.com/thopiekar/Cura
+    GIT_TAG origin/master-prepare-for-com-support
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX} -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH} -DURANIUM_SCRIPTS_DIR= -DCURA_VERSION=${CURA_VERSION} -DCURA_DEBUGMODE=${CURA_ENABLE_DEBUGMODE}
 )
 
