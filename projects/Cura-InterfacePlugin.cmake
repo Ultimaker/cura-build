770ae5335ff42d7265cb095971ef590024f9ebe6
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2017-12-08 20:43:40 +0100

    WIP: Don't include the InterfacePlugin for now.

--- projects/Cura-InterfacePlugin.cmake
+++ projects/Cura-InterfacePlugin.cmake
@@ -1,12 +1,12 @@
-# Requires only:
-# $ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64"
-
-if(BUILD_OS_WINDOWS)
-    ExternalProject_Add(InterfacePlugin
-        GIT_REPOSITORY https://github.com/thopiekar/CuraInterfacePlugin
-        GIT_TAG origin/master
-        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX} -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
-    )
-
-    SetProjectDependencies(TARGET InterfacePlugin DEPENDS Cura)
-endif()
+## Requires only:
+## $ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64"
+#
+#if(BUILD_OS_WINDOWS)
+#    ExternalProject_Add(InterfacePlugin
+#        GIT_REPOSITORY https://github.com/thopiekar/CuraInterfacePlugin
+#        GIT_TAG origin/master
+#        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX} -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
+#    )
+#
+#    SetProjectDependencies(TARGET InterfacePlugin DEPENDS Cura)
+#endif()
