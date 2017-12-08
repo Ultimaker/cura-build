aad7f160c6cae4fe82e597ac9213507a59f9f190
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2017-12-08 18:15:17 +0100

    InterfacePlugin: Another fix
    I'm beleaving to much in my copy/pasted things..

--- projects/Cura-InterfacePlugin.cmake
+++ projects/Cura-InterfacePlugin.cmake
@@ -1,11 +1,12 @@
+# Requires only:
+# $ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64"
 
-ExternalProject_Add(Cura-OctoPrint
-    GIT_REPOSITORY https://github.com/thopiekar/CuraInterfacePlugin
-    GIT_TAG origin/master
-    CMAKE_GENERATOR "NMake Makefiles"
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX} -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
-    BUILD_COMMAND nmake
-    INSTALL_COMMAND nmake install
-)
+if(BUILD_OS_WINDOWS)
+    ExternalProject_Add(InterfacePlugin
+        GIT_REPOSITORY https://github.com/thopiekar/CuraInterfacePlugin
+        GIT_TAG origin/master
+        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX} -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
+    )
 
-SetProjectDependencies(TARGET Cura-OctoPrint DEPENDS Cura)
+    SetProjectDependencies(TARGET InterfacePlugin DEPENDS Cura)
+endif()
