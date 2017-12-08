742c89eea592d1f1139d4b6eb0a4e5d7d8349702
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2017-12-08 01:28:58 +0100

    CMake: Remove Post-Processing plugin
    Can go like any other plugin to the plugin server.
    - http://software.ultimaker.com/cura/v1/plugins

--- projects/Cura-PostProcessing.cmake
+++
@@ -1,8 +0,0 @@
-
-ExternalProject_Add(Cura-PostProcessing
-    GIT_REPOSITORY https://github.com/nallath/PostProcessingPlugin
-    GIT_TAG origin/${TAG_OR_BRANCH}
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX} -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
-)
-
-SetProjectDependencies(TARGET Cura-PostProcessing DEPENDS Cura)
