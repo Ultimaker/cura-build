dd2e3efaf496e4558673afff51c5c6f959c7233d
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2017-12-08 01:29:44 +0100

    CMake: Remove OctoPrint plugin
    Can also go into the plugin server.
    - http://software.ultimaker.com/cura/v1/plugins

--- projects/Cura-OctoPrint.cmake
+++
@@ -1,8 +0,0 @@
-
-ExternalProject_Add(Cura-OctoPrint
-    GIT_REPOSITORY https://github.com/fieldOfView/OctoPrintPlugin
-    GIT_TAG origin/${TAG_OR_BRANCH}
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX} -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
-)
-
-SetProjectDependencies(TARGET Cura-OctoPrint DEPENDS Cura)
