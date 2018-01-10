7d53dde6469239926df94478c4b828e683f82fd0
Lipu Fei <lipu.fei815@gmail.com>
2018-01-10 11:59:50 +0100

    Revert "Make it possible to use different branch/tag combinations"
    This reverts commit 82bea80451b608fb7b177a3c665a10bfcc577243.
    
    I will apply this when the build system has been updated.

--- projects/cura-binary-data.cmake
+++ projects/cura-binary-data.cmake
@@ -1,7 +1,7 @@
 
 ExternalProject_Add(cura-binary-data
     GIT_REPOSITORY https://github.com/ultimaker/cura-binary-data
-    GIT_TAG origin/${CURABINARYDATA_BRANCH_OR_TAG}
+    GIT_TAG origin/${TAG_OR_BRANCH}
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX}
 )
 
