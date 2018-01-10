82bea80451b608fb7b177a3c665a10bfcc577243
Lipu Fei <lipu.fei815@gmail.com>
2018-01-10 11:58:44 +0100

    Make it possible to use different branch/tag combinations

--- projects/Uranium.cmake
+++ projects/Uranium.cmake
@@ -9,7 +9,7 @@ find_package(SciPy 0.17 REQUIRED)
 
 ExternalProject_Add(Uranium
     GIT_REPOSITORY https://github.com/ultimaker/Uranium
-    GIT_TAG origin/${TAG_OR_BRANCH}
+    GIT_TAG origin/${URANIUM_BRANCH_OR_TAG}
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX} -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
 )
 
