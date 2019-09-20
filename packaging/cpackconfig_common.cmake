a6b7d02634471e695f2616a3c212501234800035
Lipu Fei <lipu.fei815@gmail.com>
2019-09-20 16:43:46 +0200

    Add comment for MSI versioning
    CURA-6427

--- packaging/cpackconfig_common.cmake
+++ packaging/cpackconfig_common.cmake
@@ -10,9 +10,12 @@ cpack_add_component(_cura
 set(CPACK_PACKAGE_NAME "Ultimaker Cura")
 set(CPACK_PACKAGE_VENDOR "Ultimaker B.V.")
 set(CPACK_PACKAGE_HOMEPAGE_URL "https://github.com/Ultimaker/Cura")
+
+# MSI only supports version format like "x.x.x.x" where x is an integer from 0 to 65534
 set(CPACK_PACKAGE_VERSION_MAJOR ${CURA_VERSION_MAJOR})
 set(CPACK_PACKAGE_VERSION_MINOR ${CURA_VERSION_MINOR})
 set(CPACK_PACKAGE_VERSION_PATCH ${CURA_VERSION_PATCH})
+
 set(CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/packaging/cura.ico")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Ultimaker Cura - 3D Printing Software")
 set(CPACK_PACKAGE_CONTACT "Ruben Dulek <r.dulek@ultimaker.com>")
