0ba0b307877456f7ae6aa57fe4ab055c203dde3f
Lipu Fei <lipu.fei815@gmail.com>
2020-01-08 10:59:10 +0100

    WIP: Detect TTY in linux/build.sh

--- scripts/linux/build.sh
+++ scripts/linux/build.sh
@@ -36,6 +36,11 @@ CURA_CLOUD_ACCOUNT_API_ROOT="${CURA_CLOUD_ACCOUNT_API_ROOT:-https://account.ulti
 CURA_ENABLE_DEBUG_MODE="${CURA_ENABLE_DEBUG_MODE:-yes}"
 CURA_ENABLE_CURAENGINE_EXTRA_OPTIMIZATION_FLAGS="${CURA_ENABLE_CURAENGINE_EXTRA_OPTIMIZATION_FLAGS:-yes}"
 
+if [ -t 0 ]; then
+  IS_INTERACTIVE=yes
+else
+  IS_INTERACTIVE=no
+fi
 
 # Docker image to use for building the AppImage
 cura_build_env_image="ultimaker/cura-build-environment"
@@ -46,6 +51,12 @@ cd "${ROOT_DIR}"
 # Prepare the "appimages" directory
 mkdir -p appimages
 
+DOCKER_EXTRA_ARGS=""
+if [ "${IS_INTERACTIVE}" == "yes" ]; then
+  # Add -it so you can do CTRL-C in terminal if you are running in a TTY.
+  DOCKER_EXTRA_ARGS="-it ${DOCKER_EXTRA_ARGS}"
+fi
+
 # Run docker to create the AppImage
 #
 # Environment variables:
@@ -53,7 +64,8 @@ mkdir -p appimages
 #
 docker run \
   --name cura-builder \
-  -it --rm \
+  ${DOCKER_EXTRA_ARGS} \
+  --rm \
   --volume "$(pwd)":/home/ultimaker/src \
   --env CURA_APPIMAGES_OUTPUT_DIR=/home/ultimaker/src/appimages \
   --env CURA_BRANCH_OR_TAG="${CURA_BRANCH_OR_TAG}" \
