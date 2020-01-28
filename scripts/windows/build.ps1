f46773c5914c5ebd4451b6d42406e136f2f71599
Lipu Fei <lipu.fei815@gmail.com>
2020-01-28 14:48:44 +0100

    WIP: continue to fix extra args in powershell

--- scripts/windows/build.ps1
+++ scripts/windows/build.ps1
@@ -94,7 +94,7 @@ if ($BindSshVolume) {
   $dockerExtraArgs.Add("${sshPath}:C:\Users\ContainerAdministrator\.ssh")
 }
 
-& docker.exe run dockerExtraArgs:* `
+& docker.exe run $dockerExtraArgs `
   --rm `
   --volume ${repoRoot}:C:\cura-build-src `
   --volume ${outputRoot}:C:\cura-build-output `
