2981f4d6c281235447737e34325578884093558d
Mark Burton <markb@smartavionics.com>
2020-02-01 19:16:58 +0000

    Quote all usages of scriptdir so that it may contain spaces.

--- packaging/cura.sh
+++ packaging/cura.sh
@@ -2,13 +2,13 @@
 
 scriptdir=$(dirname $0)
 
-export PYTHONPATH=$scriptdir/lib/python3.5
-export QT_PLUGIN_PATH=$scriptdir/qt/plugins
-export QML2_IMPORT_PATH=$scriptdir/qt/qml
+export PYTHONPATH="$scriptdir/lib/python3.5"
+export QT_PLUGIN_PATH="$scriptdir/qt/plugins"
+export QML2_IMPORT_PATH="$scriptdir/qt/qml"
 export QT_QPA_FONTDIR=/usr/share/fonts
 export QT_XKB_CONFIG_ROOT=/usr/share/X11/xkb
 
 # Use the openssl.cnf packaged in the AppImage
-export OPENSSL_CONF=$scriptdir/openssl.cnf
+export OPENSSL_CONF="$scriptdir/openssl.cnf"
 
 cura "$@"
