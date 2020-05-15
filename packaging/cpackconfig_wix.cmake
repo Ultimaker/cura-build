ad2a0996330046fe11b191a36e9e8f1d747bd18d
Evangelos Trantos <evtrados@gmail.com>
2020-05-15 15:31:43 +0200

    Using the CURA_BUILDTYPE variable to distinguish between normal and Enterprise builds

--- packaging/cpackconfig_wix.cmake
+++ packaging/cpackconfig_wix.cmake
@@ -11,6 +11,5 @@ set(CPACK_WIX_UPGRADE_GUID "${CURA_MSI_UPGRADE_GUID}")
 set(CPACK_WIX_PRODUCT_ICON "${CMAKE_SOURCE_DIR}/packaging/cura.ico")
 set(CPACK_WIX_UI_BANNER "${CMAKE_SOURCE_DIR}/packaging/cura_banner_msi.bmp")
 set(CPACK_WIX_UI_DIALOG "${CMAKE_SOURCE_DIR}/packaging/cura_background_msi.bmp")
-set(CPACK_WIX_PROGRAM_MENU_FOLDER "Ultimaker Cura")
-
+set(CPACK_WIX_PROGRAM_MENU_FOLDER "Ultimaker Cura ${CURA_BUILDTYPE}")
 set(CPACK_WIX_TEMPLATE "${CMAKE_SOURCE_DIR}/packaging/cura.wxs")
