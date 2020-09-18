50f6d582a0fece8a6c7f2bc2ce9cb9f24f4132cd
Evangelos Trantos <evtrados@gmail.com>
2020-09-18 13:33:44 +0200

    Differentiate between build types
    - Update the display name to eliminate the extra space character whenever the build regards a specific build type
    - Use the full version for EXE installations as well
    - Added extra comments

--- packaging/cpackconfig_common.cmake
+++ packaging/cpackconfig_common.cmake
@@ -1,15 +1,23 @@
-cpack_add_component(_cura
-            DISPLAY_NAME "Ultimaker Cura ${CURA_BUILDTYPE}"
-            Description "Ultimaker Cura ${CURA_BUILDTYPE} Executable and Data Files"
-            REQUIRED
-)
+if(CURA_BUILDTYPE MATCHES "")
+    cpack_add_component(_cura
+                DISPLAY_NAME "Ultimaker Cura"
+                Description "Ultimaker Cura Executable and Data Files"
+                REQUIRED
+    )
+else()
+    cpack_add_component(_cura
+                DISPLAY_NAME "Ultimaker Cura ${CURA_BUILDTYPE}"
+                Description "Ultimaker Cura ${CURA_BUILDTYPE} Executable and Data Files"
+                REQUIRED
+    )
+endif()
 
 # ========================================
 # CPack Common Settings
 # ========================================
 
 set(CPACK_PACKAGE_NAME "Ultimaker Cura")
-if(CURA_BUILDTYPE MATCHES "Enterprise")
+if(NOT CURA_BUILDTYPE MATCHES "")
     set(CPACK_PACKAGE_NAME "Ultimaker Cura ${CURA_BUILDTYPE}")
 endif()
 string(REPLACE " " "_" CPACK_FILE_NAME_NO_SPACES "${CPACK_PACKAGE_NAME}")
@@ -22,28 +30,34 @@ set(CPACK_PACKAGE_VERSION_MAJOR ${CURA_VERSION_MAJOR})
 set(CPACK_PACKAGE_VERSION_MINOR ${CURA_VERSION_MINOR})
 set(CPACK_PACKAGE_VERSION_PATCH ${CURA_VERSION_PATCH})
 
-# Use full version x.x.x string in install directory of MSI,
-#  so that IT can easily automatically upgrade to a newer patch version
-set(CURA_FULL_VERSION "${CURA_VERSION_MAJOR}.${CURA_VERSION_MINOR}")
-if(CPACK_GENERATOR MATCHES "WIX")
-    set(CURA_FULL_VERSION "${CURA_FULL_VERSION}.${CURA_VERSION_PATCH}")
+# Use full version x.x.x string in install directory for both installers,
+# so that IT can easily automatically upgrade to a newer patch version,
+# but also uninstall only the desired EXE installation. Also differentiate
+# betweem build types
+set(CURA_FULL_VERSION "${CURA_VERSION_MAJOR}.${CURA_VERSION_MINOR}.${CURA_VERSION_PATCH}")
+if(CURA_BUILDTYPE MATCHES "")
+    set(CPACK_PACKAGE_NAME "Ultimaker Cura ${CURA_FULL_VERSION}")
+else()
+    set(CPACK_PACKAGE_NAME "Ultimaker Cura ${CURA_BUILDTYPE} ${CURA_FULL_VERSION}")
 endif()
-set(CPACK_PACKAGE_NAME "Ultimaker Cura ${CURA_BUILDTYPE} ${CURA_FULL_VERSION}")
 
 set(CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/packaging/cura.ico")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Ultimaker Cura - 3D Printing Software")
 set(CPACK_PACKAGE_CONTACT "Ruben Dulek <r.dulek@ultimaker.com>")
 set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_SOURCE_DIR}/packaging/cura_license.txt")
 
-set(CPACK_CREATE_DESKTOP_LINKS Cura "Ultimaker Cura ${CURA_FULL_VERSION}")
-set(CPACK_PACKAGE_EXECUTABLES Cura "Ultimaker Cura ${CURA_FULL_VERSION}")
-set(CPACK_PACKAGE_INSTALL_DIRECTORY "Ultimaker Cura ${CURA_FULL_VERSION}")
-if(CURA_BUILDTYPE MATCHES "Enterprise")
+# Differentiate between a normal Cura installation and that of a different build type
+if(CURA_BUILDTYPE MATCHES "")
+    set(CPACK_CREATE_DESKTOP_LINKS Cura "Ultimaker Cura ${CURA_FULL_VERSION}")
+    set(CPACK_PACKAGE_EXECUTABLES Cura "Ultimaker Cura ${CURA_FULL_VERSION}")
+    set(CPACK_PACKAGE_INSTALL_DIRECTORY "Ultimaker Cura ${CURA_FULL_VERSION}")
+else()
     set(CPACK_CREATE_DESKTOP_LINKS Cura "Ultimaker Cura ${CURA_BUILDTYPE} ${CURA_FULL_VERSION}")
     set(CPACK_PACKAGE_EXECUTABLES Cura "Ultimaker Cura ${CURA_BUILDTYPE} ${CURA_FULL_VERSION}")
     set(CPACK_PACKAGE_INSTALL_DIRECTORY "Ultimaker Cura ${CURA_BUILDTYPE} ${CURA_FULL_VERSION}")
 endif()
 
+# Forcing the CPack System Name to be either win32 or win64 for consistency purposes
 if(CMAKE_SYSTEM_PROCESSOR MATCHES "AMD64")
     set(CPACK_SYSTEM_NAME "win64")
 else()
