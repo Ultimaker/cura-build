24b0e08bd4ebaac431a0ebee9045d4d5eeeb25a5
Evangelos Trantos <evtrados@gmail.com>
2020-09-21 22:14:46 +0200

    Using STREQUAL instead of MATCHES for better results.

--- packaging/cpackconfig_common.cmake
+++ packaging/cpackconfig_common.cmake
@@ -1,4 +1,4 @@
-if(CURA_BUILDTYPE MATCHES "")
+if(CURA_BUILDTYPE STREQUAL "")
     cpack_add_component(_cura
                 DISPLAY_NAME "Ultimaker Cura"
                 Description "Ultimaker Cura Executable and Data Files"
@@ -16,8 +16,9 @@ endif()
 # CPack Common Settings
 # ========================================
 
-set(CPACK_PACKAGE_NAME "Ultimaker Cura")
-if(NOT CURA_BUILDTYPE MATCHES "")
+if(CURA_BUILDTYPE STREQUAL "")
+    set(CPACK_PACKAGE_NAME "Ultimaker Cura")
+else()
     set(CPACK_PACKAGE_NAME "Ultimaker Cura ${CURA_BUILDTYPE}")
 endif()
 string(REPLACE " " "_" CPACK_FILE_NAME_NO_SPACES "${CPACK_PACKAGE_NAME}")
@@ -35,7 +36,7 @@ set(CPACK_PACKAGE_VERSION_PATCH ${CURA_VERSION_PATCH})
 # but also uninstall only the desired EXE installation. Also differentiate
 # betweem build types
 set(CURA_FULL_VERSION "${CURA_VERSION_MAJOR}.${CURA_VERSION_MINOR}.${CURA_VERSION_PATCH}")
-if(CURA_BUILDTYPE MATCHES "")
+if(CURA_BUILDTYPE STREQUAL "")
     set(CPACK_PACKAGE_NAME "Ultimaker Cura ${CURA_FULL_VERSION}")
 else()
     set(CPACK_PACKAGE_NAME "Ultimaker Cura ${CURA_BUILDTYPE} ${CURA_FULL_VERSION}")
@@ -47,7 +48,7 @@ set(CPACK_PACKAGE_CONTACT "Ruben Dulek <r.dulek@ultimaker.com>")
 set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_SOURCE_DIR}/packaging/cura_license.txt")
 
 # Differentiate between a normal Cura installation and that of a different build type
-if(CURA_BUILDTYPE MATCHES "")
+if(CURA_BUILDTYPE STREQUAL "")
     set(CPACK_CREATE_DESKTOP_LINKS Cura "Ultimaker Cura ${CURA_FULL_VERSION}")
     set(CPACK_PACKAGE_EXECUTABLES Cura "Ultimaker Cura ${CURA_FULL_VERSION}")
     set(CPACK_PACKAGE_INSTALL_DIRECTORY "Ultimaker Cura ${CURA_FULL_VERSION}")
