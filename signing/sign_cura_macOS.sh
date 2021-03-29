c37ed42d5d7e490296423852d509bb730f614c89
evtrados <evtrados@gmail.com>
2021-03-29 21:11:01 +0200

    Add the correct relative path to the entitlements file.

--- signing/sign_cura_macOS.sh
+++ signing/sign_cura_macOS.sh
@@ -5,10 +5,10 @@ echo "===================================="
 echo "Signing the libraries of Cura . . ."
 echo "===================================="
 find ./Ultimaker\ Cura.app -name "*.dylib" -exec codesign --verbose --force --sign $codesign_identity {} \;
-find ./Ultimaker\ Cura.app -name "*.so" -exec codesign --entitlements cura.entitlements --verbose --force -o runtime --sign $codesign_identity {} \;
+find ./Ultimaker\ Cura.app -name "*.so" -exec codesign --entitlements ../../signing/cura.entitlements --verbose --force -o runtime --sign $codesign_identity {} \;
 
 # Sign the application itself
 echo "===================================="
 echo "Signing the Ultimaker Cura.app . . ."
 echo "===================================="
-codesign --entitlements cura.entitlements --verbose --force --deep -o runtime --sign $codesign_identity "Ultimaker Cura.app"
+codesign --entitlements ../../signing/cura.entitlements --verbose --force --deep -o runtime --sign $codesign_identity "Ultimaker Cura.app"
