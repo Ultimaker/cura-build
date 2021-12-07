447c5629374b695d779adf0e4cf9347dc5d4beeb
Evangelos Trantos <evtrados@gmail.com>
2021-12-07 20:51:38 +0100

    Correcting change that needed to happen on other branch

--- cmake/FindPyQt.cmake
+++ cmake/FindPyQt.cmake
@@ -2,7 +2,7 @@
 
 include(FindPythonPackage)
 
-FindPythonPackage(PACKAGE_NAME "PyQt" MODULE_NAME "PyQt6.QtCore" VERSION_PROPERTY "PYQT_VERSION_STR")
+FindPythonPackage(PACKAGE_NAME "PyQt" MODULE_NAME "PyQt5.QtCore" VERSION_PROPERTY "PYQT_VERSION_STR")
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(PyQt REQUIRED_VARS PyQt_PATH VERSION_VAR PyQt_VERSION)
