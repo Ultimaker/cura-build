31d74ff728623be0c0c49f77ed4d395a4b4429e7
Jelle Spijker <j.spijker@ultimaker.com>
2022-03-11 10:45:24 +0100

    Variables can now be set in uniform method
    Priority order with fallthrough:
    1. CMake command argument e.q. `-DCURA_VERSION_MAJOR`
    2. CMake variable set in CMakeList e.q. `set(CURA_VERSION_MAJOR 5)`
    3. Environmental variable `export CURA_VERSION_MAJOR=5`
    4. Default value
    5. Empty value except when the optional REQUIRED keyword is used. Throws error
    
    basic usage:
    
    ```cmake
    GetFromEnvironmentOrCache(
            NAME
                <name_of_the_variable>
            DEFAULT <optional>
                <default_value>
            DESCRIPTION <optional, defaults to empty string>
                "description of the variable"
            BOOL <optional defaults to STRING>
            REQUIRED <optional, throws error when variable couldn't be set>)
    ```
    
    Contributes to CURA-8640

--- cmake/GetFromEnvironmentOrCache.cmake
+++ cmake/GetFromEnvironmentOrCache.cmake
@@ -1,9 +1,56 @@
-# Copyright 2016 Ultimaker
+# Copyright 2022 Ultimaker
 
-macro(GetFromEnvironmentOrCache VARIABLE_NAME DEFAULT_VALUE CACHE_TYPE CACHE_DESCRIPTION)
-    set(${VARIABLE_NAME} ${DEFAULT_VALUE} CACHE ${CACHE_TYPE} ${CACHE_DESCRIPTION})
+function(GetFromEnvironmentOrCache)
+    set(options BOOL FILEPATH PATH STRING INTERNAL REQUIRED)
+    set(oneValueArgs NAME DEFAULT DESCRIPTION)
+    set(multiValueArgs )
+    cmake_parse_arguments(VAR "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    if(DEFINED ENV{${VARIABLE_NAME}})
-        set(${VARIABLE_NAME} $ENV{${VARIABLE_NAME}} CACHE ${CACHE_TYPE} ${CACHE_DESCRIPTION} FORCE)
+    if(NOT VAR_NAME)
+        message(FATAL_ERROR "NAME is a required keyword")
     endif()
-endmacro(GetFromEnvironmentOrCache)
+
+    if(NOT DEFINED ${VAR_NAME})
+        if(DEFINED VAR_BOOL)
+            set(VAR_CACHE BOOL)
+        elseif(DEFINED VAR_FILEPATH)
+            set(VAR_CACHE FILEPATH)
+        elseif(DEFINED VAR_PATH)
+            set(VAR_CACHE PATH)
+        elseif(DEFINED VAR_STRING)
+            set(VAR_CACHE STRING)
+        elseif(DEFINED VAR_INTERNAL)
+            set(VAR_CACHE INTERNAL)
+        else()
+            set(VAR_CACHE STRING)
+        endif()
+
+        if(DEFINED ENV{${VAR_NAME})
+            set(VAR_VALUE $ENV{${VAR_NAME}})
+            message(STATUS "Using value of environment variable for ${VAR_NAME}: ${VAR_VALUE}")
+        elseif(DEFINED VAR_DEFAULT)
+            set(VAR_VALUE ${VAR_DEFAULT})
+            message(STATUS "Using default value for ${VAR_NAME}: ${VAR_VALUE}")
+        elseif(${VAR_REQUIRED})
+            message(SEND_ERROR "Could net set ${VAR_NAME}, either provide commandline arg, environment variable or default value")
+        else()
+            set(VAR_VALUE "")
+            message(STATUS "Using empty default value for ${VAR_NAME}")
+        endif()
+
+        if(NOT DEFINED VAR_DESCRIPTION)
+            set(VAR_DESCRIPTION "")
+        endif()
+
+        if(DEFINED VAR_VALUE)
+            set(${VAR_NAME}
+                    ${VAR_VALUE}
+                CACHE
+                    ${VAR_CACHE}
+                    ${VAR_DESCRIPTION}
+                FORCE)
+            endif()
+    else()
+        message(STATUS "Using CMake provided variable ${VAR_NAME}: ${${VAR_NAME}}")
+    endif()
+endfunction()
