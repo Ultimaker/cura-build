b7cb929a3b813654e5acbf1b2a8a1f96e310b8d2
Jelle Spijker <j.spijker@ultimaker.com>
2022-03-14 18:26:15 +0100

    Setting rpath for CuraEngine should be done in CuraEngine
    Don't spread out responsibility.
    The CMakeLists.txt of CuraEngine should ensure that the output
    is expected when the install command is done.
    
    Contributes to CURA-8640

--- packaging/osx.cmake
+++ packaging/osx.cmake
@@ -1,13 +1,4 @@
 add_custom_command(
-        TARGET packaging PRE_BUILD
-        COMMAND /Library/Developer/CommandLineTools/usr/bin/install_name_tool -add_rpath "@executable_path/lib" "${CMAKE_INSTALL_PREFIX}/bin/CuraEngine"
-        COMMAND /Library/Developer/CommandLineTools/usr/bin/install_name_tool -add_rpath "@executable_path/" "${CMAKE_INSTALL_PREFIX}/bin/CuraEngine"
-        COMMAND /Library/Developer/CommandLineTools/usr/bin/install_name_tool -add_rpath "${CMAKE_PREFIX_PATH}/" "${CMAKE_INSTALL_PREFIX}/bin/CuraEngine"
-        COMMAND /Library/Developer/CommandLineTools/usr/bin/install_name_tool -add_rpath "${CMAKE_PREFIX_PATH}/lib/" "${CMAKE_INSTALL_PREFIX}/bin/CuraEngine"
-        COMMENT "Modify RPATH for CuraEngine to libArcus"
-)
-
-add_custom_command(
         TARGET packaging POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E remove_directory ${CMAKE_BINARY_DIR}/build
         COMMAND ${CMAKE_COMMAND} -E env "PYTHONPATH=$PYTHONPATH:\"${CMAKE_PREFIX_PATH}/lib/python3.10/site-packages/:${CMAKE_INSTALL_PREFIX}/lib/python3.10/site-packages/\"" ${CMAKE_PREFIX_PATH}/bin/pyinstaller --add-data "${CMAKE_INSTALL_PREFIX}/lib/cura/plugins:plugins" --add-data "${CMAKE_INSTALL_PREFIX}/lib/uranium/plugins:plugins" ${CMAKE_INSTALL_PREFIX}/bin/cura_app.py --collect-all cura --collect-all UM --add-data "${CMAKE_INSTALL_PREFIX}/share/cura/resources:resources" --add-data "${CMAKE_INSTALL_PREFIX}/share/uranium/resources:resources" --add-data "${CMAKE_INSTALL_PREFIX}/lib/python3.10/site-packages/UM/Qt/qml/UM/:resources/qml/UM/"
